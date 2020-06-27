e030ac632f9aed96539bb753b8259802e96da838
Viyaan Jhiingade <30381782+Viyaan@users.noreply.github.com>
2020-06-27 19:56:50 +0530

    Add code

---
+++ src/main/java/com/serverless/javasdk/function/S3ImageResizeHandler.java
@@ -0,0 +1,180 @@
+package com.serverless.javasdk.function;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+
+/**
+ * The following is example Java code that reads incoming Amazon S3 events and
+ * creates a thumbnail. Note that it implements the RequestHandler interface
+ * provided in the aws-lambda-java-core library. Therefore, at the time you
+ * create a Lambda function you specify the class as the handler (that is,
+ * example.handler). For more information about using interfaces to provide a
+ * handler, see Handler interfaces.
+ * 
+ * The S3Event type that the handler uses as the input type is one of the
+ * predefined classes in the aws-lambda-java-events library that provides
+ * methods for you to easily read information from the incoming Amazon S3 event.
+ * The handler returns a string as output.
+ */
+public class S3ImageResizeHandler implements RequestHandler<S3Event, String> {
+	private static final float MAX_WIDTH = 100;
+	private static final float MAX_HEIGHT = 100;
+	private final String JPG_TYPE = (String) "jpg";
+	private final String JPG_MIME = (String) "image/jpeg";
+	private final String PNG_TYPE = (String) "png";
+	private final String PNG_MIME = (String) "image/png";
+	private static final String FROM = "viyaan.86@gmail.com";
+	private static final String TO = "viyaan.86@gmail.com";
+	private static final String CONFIGSET = "ConfigSet";
+	private static  String SUBJECT = "New Image Uploaded";
+
+	// The email body for recipients with non-HTML email clients.
+	  static final String TEXTBODY = "This email was sent through Amazon SES "
+	      + "using the AWS SDK for Java.";
+	
+
+	public String handleRequest(S3Event s3event, Context context) {
+		try {
+			S3EventNotificationRecord record = s3event.getRecords().get(0);
+
+			String srcBucket = record.getS3().getBucket().getName();
+
+			// Object key may have spaces or unicode non-ASCII characters.
+			String srcKey = record.getS3().getObject().getUrlDecodedKey();
+
+			String dstBucket = srcBucket + "-resized";
+			String dstKey = "resized-" + srcKey;
+
+			// Sanity check: validate that source and destination are different
+			// buckets.
+			if (srcBucket.equals(dstBucket)) {
+				System.out.println("Destination bucket must not match source bucket.");
+				return "";
+			}
+
+			// Infer the image type.
+			Matcher matcher = Pattern.compile(".*\\.([^\\.]*)").matcher(srcKey);
+			if (!matcher.matches()) {
+				System.out.println("Unable to infer image type for key " + srcKey);
+				return "";
+			}
+			String imageType = matcher.group(1);
+			if (!(JPG_TYPE.equals(imageType)) && !(PNG_TYPE.equals(imageType))) {
+				System.out.println("Skipping non-image " + srcKey);
+				return "";
+			}
+
+			// Download the image from S3 into a stream
+			AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+			S3Object s3Object = s3Client.getObject(new GetObjectRequest(srcBucket, srcKey));
+			InputStream objectData = s3Object.getObjectContent();
+
+			// Read the source image
+			BufferedImage srcImage = ImageIO.read(objectData);
+			int srcHeight = srcImage.getHeight();
+			int srcWidth = srcImage.getWidth();
+			// Infer the scaling factor to avoid stretching the image
+			// unnaturally
+			float scalingFactor = Math.min(MAX_WIDTH / srcWidth, MAX_HEIGHT / srcHeight);
+			int width = (int) (scalingFactor * srcWidth);
+			int height = (int) (scalingFactor * srcHeight);
+
+			BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			Graphics2D g = resizedImage.createGraphics();
+			// Fill with white before applying semi-transparent (alpha) images
+			g.setPaint(Color.white);
+			g.fillRect(0, 0, width, height);
+			// Simple bilinear resize
+			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g.drawImage(srcImage, 0, 0, width, height, null);
+			g.dispose();
+
+			// Re-encode image to target format
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			ImageIO.write(resizedImage, imageType, os);
+			InputStream is = new ByteArrayInputStream(os.toByteArray());
+			// Set Content-Length and Content-Type
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentLength(os.size());
+			if (JPG_TYPE.equals(imageType)) {
+				meta.setContentType(JPG_MIME);
+			}
+			if (PNG_TYPE.equals(imageType)) {
+				meta.setContentType(PNG_MIME);
+			}
+
+			// Uploading to S3 destination bucket
+			System.out.println("Writing to: " + dstBucket + "/" + dstKey);
+			try {
+				s3Client.putObject(dstBucket, dstKey, is, meta);
+				String subject = "Successfully resized " + srcBucket + "/" + srcKey + " and uploaded to " + dstBucket
+						+ "/" + dstKey;
+				notifyEmail(subject);
+			} catch (AmazonServiceException e) {
+				System.err.println(e.getErrorMessage());
+				System.exit(1);
+			}
+			return "Ok";
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void notifyEmail(String subject) {
+		    String HTMLBODY = "<h1>Amazon SES (AWS SDK for Java)</h1>"
+				  +"<H2>" + subject +"</H2>"
+			      + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+			      + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>" 
+			      + "AWS SDK for Java</a>";
+		 AmazonSimpleEmailService client =  
+		          AmazonSimpleEmailServiceClientBuilder.standard()
+		          // Replace US_EAST_1 with the AWS Region you're using for
+		          // Amazon SES.
+		            .withRegion(Regions.US_EAST_1).build();
+		      SendEmailRequest request = new SendEmailRequest()
+		          .withDestination(
+		              new Destination().withToAddresses(TO))
+		          .withMessage(new Message()
+		              .withBody(new Body()
+		                  .withHtml(new Content()
+		                      .withCharset("UTF-8").withData(HTMLBODY))
+		                  .withText(new Content()
+		                      .withCharset("UTF-8").withData(TEXTBODY)))
+		              .withSubject(new Content()
+		                  .withCharset("UTF-8").withData(SUBJECT)))
+		          .withSource(FROM);
+
+		      client.sendEmail(request);
+		      System.out.println("Email sent!");
+		
+	}
+}
